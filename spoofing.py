dd3b76c958c185c31880c8a260331535ae3fd67a
nubi12336 <syislu62823@kaist.ac.kr>
2021-04-09 21:00:11 +0900

    d

--- spoofing.py
+++ spoofing.py
@@ -6,7 +6,7 @@ B= "192.168.20.1"
 C= RandShort()
 D= 53
 
-data= ('32 ff 00 58 00 00 00 01 28 db 00 00 45 00 00 54 00 00 40 00 40 00 5e')
+data= ('32 ff 00 58 00 00 00 01 28 db 00 00 45 00 00 54 00 00 40 00 40 00')
 data_list= data.split(" ")
 data_s= ''.join(data_list).decode('hex')
 
